1e7743d972d9752058d0e5a27c98be771c5c918c
rafael <rafael.rac.mg@gmail.com>
2020-09-05 00:22:13 -0300

    feat: botton icons

--- github_watch_releases/src/navigations/presenter/bottom.tab.navigation.tsx
+++ github_watch_releases/src/navigations/presenter/bottom.tab.navigation.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const BottomTab = createMaterialBottomTabNavigator();
 
@@ -14,8 +15,22 @@ import {SearchScreen} from '../../search/presenter/search.screen';
 export const BottomTabNavigation: React.FC = () => {
   return (
     <BottomTab.Navigator>
-      <BottomTab.Screen name="dashboard" component={Dashboard} />
-      <BottomTab.Screen name="search" component={SearchScreen} />
+      <BottomTab.Screen
+        name="dashboard"
+        component={Dashboard}
+        options={{
+          tabBarLabel: 'Dashboard',
+          tabBarIcon: ({color}) => <MaterialCommunityIcons name="page-next" color={color} size={26} />,
+        }}
+      />
+      <BottomTab.Screen
+        name="search"
+        component={SearchScreen}
+        options={{
+          tabBarLabel: 'Search',
+          tabBarIcon: ({color}) => <MaterialCommunityIcons name="page-next" color={color} size={26} />,
+        }}
+      />
     </BottomTab.Navigator>
   );
 };
