0a8e03824ad321ed97443299c09dbca0dbb9a217
rafael <rafael.rac.mg@gmail.com>
2020-09-20 11:21:00 -0300

    chore: create native service to communicate with RN

---
+++ android/app/src/main/java/com/myapp/SnifferEventService.java
@@ -0,0 +1,21 @@
+package com.myapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+
+import com.facebook.react.HeadlessJsTaskService;
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.jstasks.HeadlessJsTaskConfig;
+
+public class SnifferEventService extends HeadlessJsTaskService {
+    @Nullable
+    protected HeadlessJsTaskConfig getTaskConfig(Intent intent) {
+        Bundle extras = intent.getExtras();
+        return new HeadlessJsTaskConfig(
+                "Sniffer",
+                extras != null ? Arguments.fromBundle(extras) : Arguments.createMap(),
+                5000,
+                true);
+    }
+}
